1b8d0f326269e477cf8fef953f584f1f86ad446e
Alberto Lorente Galé <albertolorente97@gmail.com>
2024-04-19 10:28:29 +0200

    updated ap key path

--- RorschIA_app/functions.py
+++ RorschIA_app/functions.py
@@ -96,7 +96,7 @@ def get_responses(raw_text):
 
     # add language detector is it english or french? - if 
 
-    with open("DEEPL_API_KEY.txt", "r") as f:
+    with open("RorschIA_app/DEEPL_API_KEY.txt", "r") as f:
       API_KEY = f.read()
     
     # print("API Key Found!")
